371d0d0b5356acd3ff5a84a21fb25b210749e17e
rodoliv98 <rdgoliv18@gmail.com>
2025-03-22 21:40:31 -0300

    Fixed password message

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -38,6 +38,12 @@ const Register = () => {
                 setPasswordError('');
             }
         }
+
+        if (name === 'confirmPassword' || name === 'password') {
+            if (formData.password === value || formData.confirmPassword === value) {
+                setMessage('');
+            }
+        }
     };
 
     const handleSubmit = async (e) => {
@@ -138,7 +144,11 @@ const Register = () => {
                             className="bg-gray-700 text-white w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
                         />
                     </div>
-                    <button type="submit" className="w-full px-4 py-2 font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    <button 
+                        type="submit" 
+                        className="w-full px-4 py-2 font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed"
+                        //disabled={isSubmitting} // Disable button while submitting
+                    >
                         Register
                     </button>
                     {isSubmitting && (
