e682a00412b22fa2922eee8076a93bff30478031
rodoliv98 <rdgoliv18@gmail.com>
2025-05-20 11:31:39 -0300

    Fixed prod url

--- intercepter/intercepter.js
+++ intercepter/intercepter.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-/* import 'dotenv/config';
-process.env.NODE_ENV === 'production' ? process.env.PROD_URL : process.env.DEV_URL
-console.log(process.env.NODE_ENV) */
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://e-commerce-api-ts.onrender.com',
     withCredentials: true
 });
 
