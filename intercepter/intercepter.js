71300dafbccffdc659c55221b49aa7bd9d2d38ee
rodoliv98 <rdgoliv18@gmail.com>
2025-06-13 12:38:11 -0300

    Changed api url

--- intercepter/intercepter.js
+++ intercepter/intercepter.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://e-commerce-api-ts.onrender.com',
+    baseURL: 'advisory-sadella-rodoliv98-bfcde16c.koyeb.app',
     withCredentials: true
 });
 
